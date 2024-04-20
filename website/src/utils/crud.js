d623d6c30ee8a033dde77d5384b7db69e13b5e8a
Ronaldrc <ronrchim@gmail.com>
2024-04-20 04:11:16 -0400

    Add credentials

--- website/src/utils/crud.js
+++ website/src/utils/crud.js
@@ -1,6 +1,10 @@
 const AWS = require('aws-sdk');
 
-AWS.config.update({region:'us-east-1'});
+AWS.config.update({
+  region: 'us-east-1',
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+});
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
