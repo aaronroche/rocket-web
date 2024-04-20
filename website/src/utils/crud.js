9835b08af43db9c13f19691bb2f76948eb400d75
Ronaldrc <ronrchim@gmail.com>
2024-04-20 01:50:14 -0400

    Removed credentials in crud.js - cdk gives permission

--- website/src/utils/crud.js
+++ website/src/utils/crud.js
@@ -1,15 +1,6 @@
 const AWS = require('aws-sdk');
 
-
-AWS.config.update({
-  region: 'us-east-1',
-  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
-});
-
-
 const docClient = new AWS.DynamoDB.DocumentClient();
-
 
 const getAllItems = async (tableName) => {
     const params = {
